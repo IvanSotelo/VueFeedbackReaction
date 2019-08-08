095a86d7a532c6305d8a70438a11fe06b027332c
Ivan Sotelo <isotelo@controlla.com.mx>
2019-08-07 22:00:43 -0600

    fix: demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  #app
-    vue-feedback-reaction(v-model="feedback")
+  #app(:style="style")
+    vue-feedback-reaction.feedback(v-model="feedback")
 </template>
 
 <script>
@@ -14,15 +14,38 @@ export default {
     return {
       feedback: null
     }
+  },
+  computed: {
+    style () {
+      return {
+        background: this.feedback === '1' ? '#5B37B7'
+          : this.feedback === '2' ? '#BE3099'
+            : this.feedback === '3' ? '#E39A28'
+              : this.feedback === '4' ? '#11aa50'
+                : '#1194AA',
+        transition: 'all 500ms linear'
+      }
+    }
   }
 }
 </script>
 <style lang="stylus">
+body
+  margin 0
 #app
   font-family 'Avenir', Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
-  color #2c3e50
-  margin-top 60px
+  display flex
+  flex-direction column
+  height 100vh
+  align-items center
+  justify-content center
+  .feedback
+    padding 10px
+    text-align center
+    display inline-flex
+    background white
+    border-radius 12px
 </style>
